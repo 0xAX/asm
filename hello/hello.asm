70b764f91f3be00b053825c7b4c08a8e557bfc46
0xAX <testtest.89@mail.ru>
2014-08-29 18:03:32 +0600

    hello world with nasm

---
+++ hello/hello.asm
@@ -0,0 +1,24 @@
+section .data
+    msg db      "hello, world!"
+
+section .text
+
+global _start
+
+_start:
+    ;; write syscal
+    mov     rax, 1
+    ;; file descritor, standard output
+    mov     rdi, 1
+    ;; message address
+    mov     rsi, msg
+    ;; length of message
+    mov     rdx, 15
+    ;; call write syscall
+    syscall
+
+    ;;
+    mov    rax, 60
+    mov    rdi, 0
+
+    syscall
