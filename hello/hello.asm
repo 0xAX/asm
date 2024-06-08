e815b18fdd4eff973f89cec5b09c8da1bb6409f5
Alexander Kuleshov <kuleshovmail@gmail.com>
2024-06-08 18:30:29 +0500

    Add comments to the hello world program
    Signed-off-by: Alexander Kuleshov <kuleshovmail@gmail.com>

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,23 +1,28 @@
+;; Definition of the `data` section
 section .data
-	msg	db "hello, world!",`\n`
+    ;; String `msg` contant with the value `hello world!`
+    msg db      "hello, world!"
 
+;; Definition of the text section
 section .text
-	global	_start
+    ;; Reference to the entry point of our program
+    global _start
 
+;; entry point
 _start:
-	;; write syscall
-	mov	rax, 1
-	;; file descriptor, standard output
-	mov	rdi, 1
-	;; message address
-	mov	rsi, msg
-	;; length of message
-	mov	rdx, 14
-	;; call write syscall
-	syscall
-
-	;; exit
-	mov	rax, 60
-	mov	rdi, 0
-
-	syscall
+    ;; Number of the system call. 1 - `sys_write`.
+    mov     rax, 1
+    ;; The first argument of the `sys_write` system call.
+    mov     rdi, 1
+    ;; The second argument of the `sys_write` system call.
+    mov     rsi, msg
+    ;; The third argument of the `sys_write` system call.
+    mov     rdx, 13
+    ;; Call the `sys_write` system call.
+    syscall
+    ;; Number of the system call. 60 - `sys_exit`.
+    mov    rax, 60
+    ;; The first argument of the `sys_exit` system call.
+    mov    rdi, 0
+    ;; Call the `sys_exit` system call.
+    syscall
