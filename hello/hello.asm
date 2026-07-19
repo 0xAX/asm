8dd396656ae50c43d7c6ef965611ae988fd3eae3
Nguyen Phuoc Thanh Lam <phuocthanhlamnguyen@gmail.com>
2026-07-19 17:39:22 +0700

    Change patch-1 code
    Change the code to make it easier to get the size of the variable, as well as I added more functions into the code

--- hello/hello.asm
+++ hello/hello.asm
@@ -1,30 +1,37 @@
 ;; Definition of the `data` section
+
+;; Run it in UNIX or UNIX-like OS, and clearly don't compile on DOS mode
 section .data
         ;; String variable with the value `hello world!`. 
-        ;; `10` is the ASCII code of the new line symbol.
-        msg db "hello, world!", 10
+        ;; Also, 0ah is the way to make the new line symbol.
+        msg db "hello world!", 0ah ;;0ah is newline in ASM 
+        ;;The size of the string/variable (14 charaters)
+        .size equ $ - msg
 
 ;; Definition of the text section
 section .text
         ;; Reference to the entry point of our program
         global _start
-
 ;; Entry point
 _start:
-        ;; Specify the number of the system call (1 is `sys_write`).
-        mov rax, 1
-        ;; Set the first argument of `sys_write` to 1 (`stdout`).
-        mov rdi, 1
-        ;; Set the second argument of `sys_write` to the reference of the `msg` variable.
-        mov rsi, msg
-        ;; Set the third argument of `sys_write` to the length of the `msg` variable's value (14 bytes).
-        mov rdx, 14
-        ;; Call the `sys_write` system call.
-        syscall
+.syswritefunc:
+;Specify the number of syscall (sys_write)
+mov rax, 1
+;First argument, to let the computer know that it's an output(stdout)
+mov rdi, 1
+;Second argument, used to refernce the msg variable
+mov rsi, msg
+;To let the computer know the size of msg
+mov rdx, msg.size
+;Call the 'sys_write' call
+syscall
 
-        ;; Specify the number of the system call (60 is `sys_exit`).
-        mov rax, 60
-        ;; Set the first argument of `sys_exit` to 0. The 0 status code is success.
-        mov rdi, 0
-        ;; Call the `sys_exit` system call.
-        syscall
+jmp .exit
+
+.exit:
+;To let the computer know the number of syscall (sys_exit)
+mov rax, 60
+;To let it know it's a success
+mov rdi, 0
+;Call the 'sys_exit' call
+syscall
